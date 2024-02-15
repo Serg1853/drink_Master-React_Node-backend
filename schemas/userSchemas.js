6813066362ab66593d760f4a8745b5056d3d48a0
iVladyuser <korol.v09@gmail.com>
2024-02-15 16:10:13 +0100

    update User schemas

--- schemas/userSchemas.js
+++ schemas/userSchemas.js
@@ -1,11 +1,29 @@
 import Joi from "joi";
 
 export const signupSchema = Joi.object({
-  name: Joi.string().required(),
-  password: Joi.string().required(),
-  email: Joi.string().required(),
+	name: Joi.string().required(),
+	email: Joi.string().pattern(emailRegexp).required().messages({
+		"any.required": "missing required email fiel, example: ivan@gmail.com",
+	}),
+	password: Joi.string()
+		.required()
+		.min(6)
+		.messages({ "any.required": "missing required password  field" }),
 });
+
+export const verifyEmailSchema = Joi.object({
+	email: Joi.string()
+		.pattern(emailRegexp)
+		.required()
+		.messages({ "any.required": "missing required field email" }),
+});
+
 export const singinSchema = Joi.object({
-  password: Joi.string().required(),
-  email: Joi.string().required(),
+	email: Joi.string().pattern(emailRegexp).required().messages({
+		"any.required": "missing required email field, example: ivan@gmail.com",
+	}),
+	password: Joi.string()
+		.required()
+		.min(6)
+		.messages({ "any.required": "missing required password field" }),
 });
