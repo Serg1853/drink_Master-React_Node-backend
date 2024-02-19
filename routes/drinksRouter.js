b65e4e1876c45023ead692226cd1dd821643e1ef
Serg1853 <sergey24226@gmail.com>
2024-02-19 19:59:51 +0200

    add routers

--- routes/drinksRouter.js
+++ routes/drinksRouter.js
@@ -8,4 +8,20 @@ drinksRouter.get("/mainpage", getAll);
 
 drinksRouter.get("/:id", getById);
 
+drinksRouter.get("/popular");
+
+drinksRouter.get("/search");
+
+drinksRouter.post("/own/add");
+
+drinksRouter.delete("/own/remove");
+
+drinksRouter.get("/own");
+
+drinksRouter.post("/favorite/add");
+
+drinksRouter.delete("/favorite/remove");
+
+drinksRouter.get("/favorite");
+
 module.exports = drinksRouter;
