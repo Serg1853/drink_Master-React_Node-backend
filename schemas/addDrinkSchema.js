e04931ab2ab8954f388c1006a442e1bd98b0affb
Mark <markiyan.chornovil@gmail.com>
2024-02-21 15:29:53 +0100

    Update addDrinkSchema.js

--- schemas/addDrinkSchema.js
+++ schemas/addDrinkSchema.js
@@ -1,44 +1,44 @@
 const Joi = require("joi");
 
 const addDrinkSchema = Joi.object({
-	file: String,
-	drink: Joi.string()
-		.required()
-		.messages({ "any.required": "missing required name field" }),
-	description: Joi.string()
-		.required()
-		.messages({ "any.required": "missing required description field" }),
-	category: Joi.string()
-		.required()
-		.messages({ "any.required": "missing required category field" }),
-	glass: Joi.string()
-		.required()
-		.messages({ "any.required": "missing required glass field" }),
-	alcoholic: Joi.string()
-		.required()
-		.messages({ "any.required": "missing required alcohol/not alcohol field" }),
-	ingredients: Joi.array()
-		.items(
-			Joi.object({
-				_id: Joi.string().required(),
-				title: Joi.string().required().messages({
-					"any.required": "missing required ingredient name field",
-				}),
-				measure: Joi.string()
-					.required()
-					.messages({ "any.required": "missing required measure field" }),
-				ingredientId: Joi.object({
-					_id: Joi.string().required(),
-					ingredientThumb: Joi.string().uri().optional(),
-					"thumb-medium": Joi.string().uri().optional(),
-					"thumb-small": Joi.string().uri().optional(),
-				}).required(),
-			})
-		)
-		.required(),
-	instructions: Joi.string()
-		.required()
-		.messages({ "any.required": "missing required instruction field" }),
+  file: String,
+  drink: Joi.string()
+    .required()
+    .messages({ "any.required": "missing required name field" }),
+  description: Joi.string()
+    .required()
+    .messages({ "any.required": "missing required description field" }),
+  category: Joi.string()
+    .required()
+    .messages({ "any.required": "missing required category field" }),
+  glass: Joi.string()
+    .required()
+    .messages({ "any.required": "missing required glass field" }),
+  alcoholic: Joi.string()
+    .required()
+    .messages({ "any.required": "missing required alcohol/not alcohol field" }),
+  //   ingredients: Joi.array()
+  //     .items(
+  //       Joi.object({
+  //         _id: Joi.string().required(),
+  //         title: Joi.string().required().messages({
+  //           "any.required": "missing required ingredient name field",
+  //         }),
+  //         measure: Joi.string()
+  //           .required()
+  //           .messages({ "any.required": "missing required measure field" }),
+  //         ingredientId: Joi.object({
+  //           _id: Joi.string().required(),
+  //           ingredientThumb: Joi.string().uri().optional(),
+  //           "thumb-medium": Joi.string().uri().optional(),
+  //           "thumb-small": Joi.string().uri().optional(),
+  //         }).required(),
+  //       })
+  //     )
+  //     .required(),
+  //   instructions: Joi.string()
+  //     .required()
+  //     .messages({ "any.required": "missing required instruction field" }),
 });
 
 module.exports = addDrinkSchema;
