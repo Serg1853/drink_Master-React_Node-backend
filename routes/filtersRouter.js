74408eb517897e4288ad6d79e48753e91dd8a13e
iVladyuser <korol.v09@gmail.com>
2024-02-23 13:35:54 +0100

    fix filters foutes

--- routes/filtersRouter.js
+++ routes/filtersRouter.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const ctrl = require("../controllers/filtersDrinksControllers");
-
+const { authenticate } = require("../middlewares");
 const filtersDrinksRouter = express.Router();
 
-filtersDrinksRouter.get("/categories", ctrl.getCategories);
+filtersDrinksRouter.get("/categories", authenticate, ctrl.getCategories);
 
-filtersDrinksRouter.get("/glasses", ctrl.getGlasses);
+filtersDrinksRouter.get("/glasses", authenticate, ctrl.getGlasses);
 
-filtersDrinksRouter.get("/ingredients", ctrl.getIngredients);
+filtersDrinksRouter.get("/ingredients", authenticate, ctrl.getIngredients);
 
 module.exports = filtersDrinksRouter;
