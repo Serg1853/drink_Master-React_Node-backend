7c023f59ddedbd5552df48ae4d78fa043c0cda70
iVladyuser <korol.v09@gmail.com>
2024-02-26 14:16:46 +0100

    remove owner

--- controllers/drinksControllers.js
+++ controllers/drinksControllers.js
@@ -26,157 +26,157 @@ const getAll = async (req, res) => {
 };
 
 const findDrinkByFiltrs = async (req, res) => {
-  const { age } = req.user;
-  const { page = 1, limit = 10 } = req.query;
-  let skip = (page - 1) * limit;
+	const { age } = req.user;
+	const { page = 1, limit = 10 } = req.query;
+	let skip = (page - 1) * limit;
 
-  const keys = Object.keys(req.query);
-  let paramSearch = {};
-  for (const key of keys) {
-    if (key === "drink" || key === "category" || key === "ingredients.title") {
-      paramSearch = {
-        ...paramSearch,
-        [key]: { $regex: new RegExp(req.query[key], "i") },
-      };
-    }
-  }
-  //   const age = 29;
-  let searchFilters = { ...paramSearch, alcoholic: "Non alcoholic" };
-  if (age > 18) {
-    searchFilters = { ...paramSearch };
-  }
+	const keys = Object.keys(req.query);
+	let paramSearch = {};
+	for (const key of keys) {
+		if (key === "drink" || key === "category" || key === "ingredients.title") {
+			paramSearch = {
+				...paramSearch,
+				[key]: { $regex: new RegExp(req.query[key], "i") },
+			};
+		}
+	}
+	//   const age = 29;
+	let searchFilters = { ...paramSearch, alcoholic: "Non alcoholic" };
+	if (age > 18) {
+		searchFilters = { ...paramSearch };
+	}
 
-  const resultCount = await Recipe.find(searchFilters).count();
-  if (skip >= resultCount) {
-    if (resultCount < limit) {
-      skip = 0;
-    } else {
-      skip = resultCount - limit;
-    }
-  }
+	const resultCount = await Recipe.find(searchFilters).count();
+	if (skip >= resultCount) {
+		if (resultCount < limit) {
+			skip = 0;
+		} else {
+			skip = resultCount - limit;
+		}
+	}
 
-  const result = await Recipe.find(searchFilters, { Recipe }, { skip, limit });
+	const result = await Recipe.find(searchFilters, { Recipe }, { skip, limit });
 
-  if (!result || !result.length) {
-    throw HttpError(404, "Not found, try again");
-  }
+	if (!result || !result.length) {
+		throw HttpError(404, "Not found, try again");
+	}
 
-  res.json({
-    result: result,
-    count: resultCount,
-  });
+	res.json({
+		result: result,
+		count: resultCount,
+	});
 };
 
 const getById = async (req, res, next) => {
-  const { id } = req.params;
+	const { id } = req.params;
 
-  const result = await Recipe.findById({ _id: id }).populate(
-    "ingredients.ingredientId",
-    "ingredientThumb thumb-medium thumb-small"
-  );
-  if (result === null) {
-    throw HttpError(404, "Not found");
-  }
-  res.json(result);
+	const result = await Recipe.findById({ _id: id }).populate(
+		"ingredients.ingredientId",
+		"ingredientThumb thumb-medium thumb-small"
+	);
+	if (result === null) {
+		throw HttpError(404, "Not found");
+	}
+	res.json(result);
 };
 
 const addOwnDrink = async (req, res) => {
-  const { _id: owner } = req.user;
-  try {
-    const result = await Recipe.create({
-      ...req.body,
-      owner,
-      ingredients: JSON.parse(req.body.ingredients),
-    });
-    res.status(201).json(result);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
+	const { _id: owner } = req.user;
+	try {
+		const result = await Recipe.create({
+			...req.body,
+			owner,
+			ingredients: JSON.parse(req.body.ingredients),
+		});
+		res.status(201).json(result);
+	} catch (error) {
+		res.status(400).json({ message: error.message });
+	}
 };
 
 const getOwnDrink = async (req, res) => {
-  const { _id: owner } = req.user;
-  const result = await Recipe.find({ owner });
-  res.json(result);
+	const { _id: owner } = req.user;
+	const result = await Recipe.find({ owner });
+	res.json(result);
 };
 
 const removeOwnDrink = async (req, res) => {
-  const { id } = req.params;
-  console.log("id", id);
-	const { _id: owner } = req.user;
-  console.log("owner", owner);
-  const result = await Recipe.findOneAndDelete({
-    id,
-    owner,
-  });
-  if (!result) {
-    throw HttpError(404, "Not found");
-  }
-  res.status(200).json({ message: "drink deleted" });
+	const { id: drinkId } = req.params;
+	const { _id } = req.user;
+	const owner = _id.toString();
+
+	const result = await Recipe.findOneAndDelete({
+		_id: drinkId,
+		owner: owner,
+	});
+	if (!result) {
+		throw HttpError(404, "Not found or user is not the owner");
+	}
+	res.status(200).json({ message: "drink deleted" });
 };
 
 const getPopularDrinks = async (req, res) => {
-  const { age } = req.user;
+	const { age } = req.user;
 
-  let filter = {};
-  if (age < 18) {
-    filter.alcoholic = "Non alcoholic";
-  } else {
-    filter.alcoholic = "Alcoholic";
-  }
+	let filter = {};
+	if (age < 18) {
+		filter.alcoholic = "Non alcoholic";
+	} else {
+		filter.alcoholic = "Alcoholic";
+	}
 
-  const popularDrinks = await Recipe.aggregate([
-    {
-      $match: {
-        users: { $exists: true, $ne: [] },
-        ...filter,
-      },
-    },
-    {
-      $addFields: {
-        usersCount: { $size: "$users" },
-      },
-    },
-    {
-      $sort: {
-        usersCount: -1,
-      },
-    },
-  ]);
-  res.json(popularDrinks);
+	const popularDrinks = await Recipe.aggregate([
+		{
+			$match: {
+				users: { $exists: true, $ne: [] },
+				...filter,
+			},
+		},
+		{
+			$addFields: {
+				usersCount: { $size: "$users" },
+			},
+		},
+		{
+			$sort: {
+				usersCount: -1,
+			},
+		},
+	]);
+	res.json(popularDrinks);
 };
 
 const getFavorite = async (req, res) => {
-  const { _id: owner } = req.user;
-  const favoriteDrinks = await Recipe.find({ users: owner }).populate("owner");
-  res.json(favoriteDrinks);
+	const { _id: owner } = req.user;
+	const favoriteDrinks = await Recipe.find({ users: owner }).populate("owner");
+	res.json(favoriteDrinks);
 };
 
 const addFavorite = async (req, res) => {
-  const { id } = req.params;
-  const { _id: owner } = req.user;
+	const { id } = req.params;
+	const { _id: owner } = req.user;
 
-  const result = await Recipe.findByIdAndUpdate(
-    id,
-    { $addToSet: { users: owner } },
-    { new: true }
-  );
-  res.json(result);
+	const result = await Recipe.findByIdAndUpdate(
+		id,
+		{ $addToSet: { users: owner } },
+		{ new: true }
+	);
+	res.json(result);
 };
 
 const deleteFavorite = async (req, res) => {
-  const { id } = req.params;
-  const { _id } = req.user;
-  const result = await Recipe.findByIdAndUpdate(
-    id,
-    {
-      $pull: { users: _id },
-    },
-    { new: true }
-  );
-  res.json({
-    message: "Cocktail is delete",
-  });
+	const { id } = req.params;
+	const { _id } = req.user;
+	const result = await Recipe.findByIdAndUpdate(
+		id,
+		{
+			$pull: { users: _id },
+		},
+		{ new: true }
+	);
+	res.json({
+		message: "Cocktail is delete",
+	});
 };
 
 module.exports = {
